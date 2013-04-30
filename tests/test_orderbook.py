fdf3964478de3ef282307098a6ddb805a833477c
Jakub Warmuz <jakub.warmuz+github@gmail.com>
2013-04-30 16:15:11 +0100

    Fix orderbook testcases. PEP8.
    Also, test stressed classes should be imported from within test case
    rather than at top of the file continaing test case.

--- tests/test_orderbook.py
+++ tests/test_orderbook.py
@@ -1,40 +1,47 @@
 import unittest
 
+from oxberrypis.errors import OxBerryPisException
+
 from oxberrypis.orderbook import Order
 from oxberrypis.orderbook import Limit
-from oxberrypis.orderbook import LimitBook
 
-class TestOrderBook(object):
+
+class TestOrderBook(unittest.TestCase):
+
+    def create_limit_book(self):
+        from oxberrypis.orderbook import LimitBook
+        return LimitBook()
+
     def test_remove(self):
-        lims = LimitBook()
-        lims.add_buy(10,100,1)
-        self.assertEqual(lims.get_buy_head_order().order_id,1)
+        lims = self.create_limit_book()
+        lims.add_buy(10, 100, 1)
+        self.assertEqual(lims.get_buy_head_order().order_id, 1)
         lims.remove_order(1)
-        self.assertRaises(OxBerryPisException,lims.get_buy_head_order)
-        lims = LimitBook()
-        lims.add_sell(10,100,2)
-        self.assertEqual(lims.get_sell_head_order().order_id,2)
+        self.assertRaises(OxBerryPisException, lims.get_buy_head_order)
+        lims = self.create_limit_book()
+        lims.add_sell(10, 100, 2)
+        self.assertEqual(lims.get_sell_head_order().order_id, 2)
         lims.remove_order(1)
-        self.assertRaises(OxBerryPisException,lims.get_sell_head_order)
+        self.assertRaises(OxBerryPisException, lims.get_sell_head_order)
 
     def test_modify(self):
-        lims = LimitBook()
-        lims.add_buy(10,100,1)
-        self.assertEqual(lims.get_buy_head_order().limit.price,10)
-        lims.modify_order(1,11)
-        self.assertEqual(lims.get_buy_head_order().limit.price,11)
-        
-        lims.add_sell(10,100,2)
-        self.assertEqual(lims.get_sell_head_order().limit.price,10)
-        lims.modify_order(2,11)
-        self.assertEqual(lims.get_sell_head_order().order.limit.price,11)
+        lims = self.create_limit_book()
+        lims.add_buy(10, 100, 1)
+        self.assertEqual(lims.get_buy_head_order().limit.price, 10)
+        lims.modify_order(1, 11)
+        self.assertEqual(lims.get_buy_head_order().limit.price, 11)
+
+        lims.add_sell(10, 100, 2)
+        self.assertEqual(lims.get_sell_head_order().limit.price, 10)
+        lims.modify_order(2, 11)
+        self.assertEqual(lims.get_sell_head_order().order.limit.price, 11)
 
     def test_input(self):
-        lims = LimitBook()
-        lims.add_buy(10,100,1)
-        lims.add_buy(11,100,2)
-        self.assertEqual(lims.get_buy_head_order().limit.price,10)
-        lims.add_buy(9,100,3)
-        self.assertEqual(lims.get_buy_head_order().limit.price,9)
-        lims.add_buy(9,100,4)
-        self.assertEqual(lims.get_buy_head_order().order_id,3)
+        lims = self.create_limit_book()
+        lims.add_buy(10, 100, 1)
+        lims.add_buy(11, 100, 2)
+        self.assertEqual(lims.get_buy_head_order().limit.price, 10)
+        lims.add_buy(9, 100, 3)
+        self.assertEqual(lims.get_buy_head_order().limit.price, 9)
+        lims.add_buy(9, 100, 4)
+        self.assertEqual(lims.get_buy_head_order().order_id, 3)
