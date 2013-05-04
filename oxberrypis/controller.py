e44d6fd299c64988d5892023ad14be92f86f64e2
Alexander Eyers-Taylor <alex@localhost.localdomain>
2013-05-04 17:54:15 +0100

    Added Prtotcol for messages from parser and started a function to handle them

--- oxberrypis/controller.py
+++ oxberrypis/controller.py
@@ -4,7 +4,7 @@ import zmq
 
 pi_id = 0 #TODO: get unique identifier
 
-visualisation_uri = ""
+visualisation_uri = "tcp://visualmachine:portnum"
 
 context = zmq.context()
 
@@ -17,7 +17,7 @@ to_visualisation.conect(visualisation_uri)
 # orderbooks in map called order_books
 
 
-def sendNewPrice(timestamp,stock_id):
+def make_price_message(timestamp,stock_id):
     event = StockEvent()
     event.stock_id = stock_id
     event.timestamp = timestamp
@@ -28,5 +28,38 @@ def sendNewPrice(timestamp,stock_id):
         event.top_buy_price = top_buy_price
     if (top_sell_price is not None):
         event.top_sell_price = top_sell_price
-    to_visualisation.sent(event.SerializeToString())
+    
 
+msg = StockEvent()
+msg.parseFromString(from_parser.recv())
+
+def handle_message(message):
+    if(message.message_type == MessageType.ADD_MESSAGE):
+        stock_id = message.stock_id
+        order_id = message.order_id
+        limit_price = message.price
+        num_shares = message.volume
+        if (message.side = Side.BUY):
+            orderbooks[stock_id].add_order(order_id,limit_price,num_shares,"buy")
+        else:
+            orderbooks[stock_id].add_order(order_id,limit_price,num_shares,"sell")
+    elif(message.message_type == MessageType.MODIFY_MESSAGE):
+        stock_id = message.stock_id
+        order_id = message.order_id
+        limit_price = message.price
+        num_shares = message.volume
+        if (message.side = Side.BUY):
+            orderbooks[stock_id].update_order(order_id,limit_price,num_shares,"buy")
+        else:
+            orderbooks[stock_id].update_order(order_id,limit_price,num_shares,"sell")
+    elif(message.message_type == MessageType.DELETE_MESSAGE):
+        stock_id = message.stock_id
+        order_id = message.order_id
+        if (message.side = Side.BUY):
+            orderbooks[stock_id].remove_order(order_id)
+        else:
+            orderbooks[stock_id].remove_order(order_id)
+    
+
+            
+        
