252a5fb9c5d3680944c911b575b2965c617726e2
Alexander Eyers-Taylor <alex@localhost.localdomain>
2013-05-08 21:08:19 +0100

    Added Sending messages to Visualisation

--- oxberrypis/net/rpi/subscriber.py
+++ oxberrypis/net/rpi/subscriber.py
@@ -8,6 +8,64 @@ from ..proto.stock_pb2 import StockMessage
 from ...orderbook.order import Order
 from ...orderbook.book import OrderBook
 
+class ToVisualisation(object):
+    """ Send stock messages to visualisation"""
+
+
+    def _init_(self,matching_engines,visual_uri,context):
+        self.matching_engines = matching_engines
+        self.to_visualisation = context.socket(zmq.PUSH)
+        self.to_visualisation.connect(visualisation_uri)
+        self.last_top_buy = {}
+        self.last_top_sell = {}
+
+
+    def make_price_message(self,message,stock_id,stream_id):
+        """Makes the message to send to visualisation."""
+        
+        matching_engine = matching_engines[stock_id]
+        
+        top_sell_price, top_buy_price = matching_engine.get_best_orders()
+
+        if (message.type != StockMessage.TRADE and 
+            stock_id in last_top_buy and 
+            stock_id in last_top_sell and 
+            last_top_buy[stock_id] == top_buy_price and 
+            last_top_sell[stock_id] == top_sell_price):
+            return None
+
+        last_top_buy[stock_id] = top_buy_price
+        last_top_sell[stock_id] = top_sell_price
+
+        event = StockEvent()
+        event.stock_id = stock_id
+        event.timestamp_s = message.timestamp_s
+        event.timestamp_ns = message.timestamp_ns
+        event.stream_id = stream_id
+        
+        if message.type == StockMessage.TRADE:
+            visual_msg.last_trade_price = message.trade.price
+            
+        
+
+        if top_buy_price is not None:
+            event.top_buy_price = top_buy_price
+        if top_sell_price is not None:
+            top_sell_price = top_sell_price
+        return true,event
+        
+    def handle_send_visual(self,message,stock_id):
+        """Handle sending a single stock event to visualisation"""
+        visual_msg = self.make_price_message(message,stock_id,stream_id)
+        if (visual_msg is not None):
+            self.to_visualisation.send(msg.serialize_to_string())
+
+
+
+
+
+
+    
 
 class StockMessagesToOrderbook(object):
     """Stock messages handler."""
@@ -109,16 +167,17 @@ class StockMessagesToOrderbook(object):
 
 class StockMessagesSubscriber(object):
 
-    def __init__(self, context, publisher_uri, syncservice_uri,
-            handler_cls=StockMessagesToOrderbook):
+    def __init__(self, context, publisher_uri, syncservice_uri,visual_uri,
+                 stock_handler_cls=StockMessagesToOrderbook,
+                 visual_handler_cls=ToVisualisation):
         stocks = xrange(1, 50000)
 
         orderbooks = {}
         for stock in stocks:
             orderbooks[stock] = OrderBook()
 
-        self.handler = handler_cls(orderbooks)
-
+        self.stock_handler = stock_handler_cls(orderbooks)
+        self.visual_handler = visual_handler_cls(orderbooks,visual_uri,context)
         subscriptions = map(str, stocks)
 
         self.sub = SynchronizedSubscriber(
@@ -135,19 +194,25 @@ class StockMessagesSubscriber(object):
 
     def handle_data(self, data):
         symbol_index, serialized_stock_msg = data
+       
+        # Using dummy stream_id for now
+        stream_id = 0
+        
         stock_msg = StockMessage()
         stock_msg.ParseFromString(serialized_stock_msg)
         print stock_msg
-        self.handler.handle_stock_message(stock_msg)
+        stock_id = self.stock_handler.handle_stock_message(stock_msg)
+        self.visual_handler.send_to_visual(stock_msg,stock_id,stream_id)
 
 if __name__ == '__main__':
     import sys
 
     if len(sys.argv) != 3:
-        exit("USAGE: {} publisher_uri syncservice_uri".format(sys.argv[0]))
+        exit("USAGE: {} publisher_uri syncservice_uri visual_uri".format(sys.argv[0]))
 
     publisher_uri = sys.argv[1]
     syncservice_uri = sys.argv[2]
+    visual_uri = sys.argv[3]
 
     context = zmq.Context()
 
@@ -155,5 +220,7 @@ if __name__ == '__main__':
         context,
         publisher_uri,
         syncservice_uri,
+        visual_uri
     )
+    
     sub.run()
