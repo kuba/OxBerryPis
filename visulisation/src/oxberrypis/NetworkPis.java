3006271da985055f6cf20a0a75206595c467232e
Alexander Eyers-Taylor <alex@localhost.localdomain>
2013-05-09 07:28:39 +0100

    Added Dummy data for testing

--- visulisation/src/oxberrypis/NetworkPis.java
+++ visulisation/src/oxberrypis/NetworkPis.java
@@ -5,21 +5,20 @@ import org.zeromq.ZMQ.Context;
 
 import oxberrypis.net.proto.rpi.Rpi.StockEvent;
 import oxberrypis.net.proto.vis_init.VisInit.SetupVisualisation;
-
+import oxberrypis.net.proto.vis_init.VisInit;
 import com.google.protobuf.InvalidProtocolBufferException;
 
 /**
  * Class around ZMQ
  * 
  * The constructer creates the context
+ * 
  * @author alex
- *
+ * 
  */
 public class NetworkPis {
 	Context context = ZMQ.context(1);
 	ZMQ.Socket receiver = context.socket(ZMQ.PULL);
-
-	
 
 	public NetworkPis(String bind_uri) {
 		receiver.bind(bind_uri);
@@ -49,15 +48,22 @@ public class NetworkPis {
 	 * @return
 	 */
 	public SetupVisualisation getInit(String parser_uri) {
-		ZMQ.Socket fromParser = context.socket(ZMQ.REQ);
-		fromParser.connect(parser_uri);
-		fromParser.send("INIT");
-		try {
-			return SetupVisualisation.parseFrom(fromParser.recv(0));
-		} catch (InvalidProtocolBufferException e) {
-			throw new Error("Invalid Message Recieved"); // this should never
-															// happen so fail
-															// fast
-		}
+
+//		ZMQ.Socket fromParser = context.socket(ZMQ.REQ);
+//		fromParser.connect(parser_uri);
+//		fromParser.send("INIT");
+//		try {
+//			return SetupVisualisation.parseFrom(fromParser.recv(0));
+//		} catch (InvalidProtocolBufferException e) {
+//			throw new Error("Invalid Message Recieved");
+//		} // this should never happen fail fast }
+
+		// dummy data
+		return SetupVisualisation
+				.newBuilder()
+				.addMappings(
+						SetupVisualisation.Mapping.newBuilder()
+								.setSymbolMapStart(0).setSymbolMapEnd(7790)
+								.build()).build();
 	}
 }
